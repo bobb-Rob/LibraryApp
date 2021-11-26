b49c7477ccd635ef28c9da1d06a4caecc65b53b6
Robertson Akpan <robertsonakpan@gmail.com>
2021-11-26 22:27:58 +0100

    Working in progress, Add Book btn adds book to collection wrapper

--- Script.js
+++ Script.js
@@ -71,10 +71,6 @@ const bookCollection = component("div", "books-section");
 
 
   bookCollection.appendChild(collection);
-
-
-
-
 pageContainer.insertAdjacentElement("afterbegin", bookCollection);
 
 
@@ -94,21 +90,110 @@ navBar.appendChild(navBarTitle);
 pageContainer.insertAdjacentElement("afterbegin", navBar);
 
 
-
-
-
-
-// addBookForm.appendChild(author, bookTitle, pages, readStatus);
-// addBookForm.appendChild(bookTitle);
-// addBookForm.appendChild(pages);
-// addBookForm.appendChild(readStatus);
-// pageContainer.appendChild(addBookForm);
-
-// 
-
-
-
-
-
-
 console.log(pageContainer)
+
+
+// Dynamic Page Logic section
+
+// Add book button Event
+const formWrapper = document.getElementById("form-container");
+
+addBookButton.addEventListener("click", ()=>{
+  clearForm();
+  formWrapper.style.display = "block";
+  console.log("I was clicked")
+})
+
+// close form button evnt
+const formCloseBtn = formWrapper.querySelector(".close-btn");
+  formCloseBtn.addEventListener("click", ()=>{
+  formWrapper.style.display = "none";
+})
+
+// book constructor function and protoype
+
+function Book(bookTitle, author, pages, readStatus){
+  this.title = bookTitle;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+// Book.prototype.addNewBookToCollection = function(arr){
+//   arr.forEach(element => {        
+ 
+//   });
+// }
+
+Book.prototype.createNewBookHolder = function(bookTitle, Author, pages, readStatus) {
+          const newBookHolder = component("div", "new-book-holder"); 
+
+        const newbookImageWrapper = component("div", "new-img-wrapper")
+        const newbookimg = component("img", "new-book-image");
+              newbookImageWrapper.appendChild(newbookimg);
+              newBookHolder.appendChild(newbookImageWrapper);
+
+        const newBookDescription = component("div", "new-book-desc");    
+            // Book description display - using P at end of variable to know it is a paragraph element 
+              const newBookTitleP = component("p", "new-book-title");
+              newBookTitleP.textContent = `Title: ${bookTitle}`;
+              newBookDescription.appendChild(newBookTitleP);
+
+              const newAuthorP = component("p", "new-author")
+              newAuthorP.textContent = `Author: ${Author}`;
+              newBookDescription.appendChild(newAuthorP);
+
+              const newPagesP = component("p", "new-no-of-pages");
+              newPagesP.textContent = `Pages: ${pages}`;
+              newBookDescription.appendChild(newPagesP);
+
+              const newReadStatusP = component("p", "new-read-status");
+              newReadStatusP.textContent = `Read Status: ${readStatus}`;
+              newBookDescription.appendChild(newReadStatusP);
+
+              const ReadNowBtn = component("button", "read-now-btn")
+              ReadNowBtn.textContent = "Read now";
+              newBookDescription.appendChild(ReadNowBtn);
+    newBookHolder.appendChild(newBookDescription);
+   
+return newBookHolder;
+}
+
+console.log(Book.prototype)
+
+
+
+// Form Submit Logic
+let myLibrary = [];
+
+  const bookName = document.getElementById("author-name");
+  const bookAuthor = document.getElementById("book-title");
+  const pagesCount = document.getElementById("no-of-pages");
+  const readStatus = document.getElementById("read-status");
+  
+  
+  function clearForm(){
+    bookName.value = ""; 
+    bookAuthor.value = ""; 
+    pagesCount.value = "";
+    readStatus.value = "";
+    };
+
+    // form submission
+formWrapper.addEventListener("submit", (e)=>{
+  e.preventDefault();    
+const newBook = new Book(bookName.value, bookAuthor.value, pagesCount.value, readStatus.value);
+console.log(newBook.title);
+console.log(newBook.author);
+
+myLibrary.push(newBook);
+console.log(myLibrary);
+
+const bookHolder = newBook.createNewBookHolder(newBook.title, newBook.Author, newBook.pages, newBook.readStatus);
+collectionHolder.appendChild(bookHolder);
+// newBook.addNewBookToCollection(myLibrary);
+formWrapper.style.display = "none";
+clearForm();
+})
+
+
